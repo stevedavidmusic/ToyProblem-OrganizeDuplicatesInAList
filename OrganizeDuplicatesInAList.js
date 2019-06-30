78ac3462caa5ef4d72b0445ffce52f277b6e65d5
Your Name <you@example.com>
2019-06-29 18:28:20 -0700

    Solved

---
+++ OrganizeDuplicatesInAList.js
@@ -0,0 +1,25 @@
+// Sam is an avid collector of numbers. Every time he finds a new number he throws it on the top of his number-pile. Help Sam organise his collection so he can take it to the International Number Collectors Conference in Cologne.
+
+// Given an array of numbers, your function should return an array of arrays, where each subarray contains all the duplicates of a particular number. Subarrays should be in the same order as the first occurence of the number they contain:
+
+// group([3, 2, 6, 2, 1, 3])
+// >>> [[3, 3], [2, 2], [6], [1]]
+// Assume the input is always going to be an array of numbers. If the input is an empty array, an empty array should be returned.
+
+function group(arr) {
+    var singles = [...new Set(arr)]
+    var answer =[]
+    if(arr === []){
+      return []
+    }
+    for(let i = 0; i < singles.length; i++){
+      var hold = []
+      for(let k = 0; k < arr.length; k++){
+        if(singles[i] === arr[k]){
+          hold.push(singles[i])
+        }
+      }
+      answer.push(hold)
+    } return answer
+  }
+  
